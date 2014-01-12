4e444c7c14a77b4820043af4bf352bd1adfe7eab
carlo scarioni <carlo.scarioni@gmail.com>
2014-01-12 16:14:04 +0000

    speech lights on

---
+++ speech_recognition/speech_recognition.ino
@@ -0,0 +1,46 @@
+
+char inData[20];
+char inChar=-1; 
+byte index = 0;
+int led = 13;
+ 
+void setup() {
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
+}
+ 
+void loop() {
+    if(Comp("lightsOn")==0){
+       digitalWrite(led, HIGH);
+       Serial.println("turning on");
+    }
+    else if(Comp("lightsOff")==0){       
+       digitalWrite(led, LOW);
+       Serial.println("turning off");
+    }   
+}
+ 
+char Comp(char* This) {
+    while (Serial.available() > 0) // Don't read unless
+                                   // there you know there is data
+    {
+        if(index < 19) // One less than the size of the array
+        {
+            inChar = Serial.read(); // Read a character
+            inData[index] = inChar; // Store it
+            index++; // Increment where to write next
+            inData[index] = '\0'; // Null terminate the string
+        }
+    }
+ 
+    if (strcmp(inData,This)  == 0) {
+        for (int i=0;i<19;i++) {
+            inData[i]=0;
+        }
+        index=0;
+        return(0);
+    }
+    else {
+        return(1);
+    }
+}
